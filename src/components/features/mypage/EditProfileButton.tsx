f7abea387ea50409891c8a687737f23ea27433f4
joycie416 <joycie416@gmail.com>
2024-10-13 22:09:43 +0900

    [add] 마이페이지 기능 추가

--- src/components/features/mypage/EditProfileButton.tsx
+++ src/components/features/mypage/EditProfileButton.tsx
@@ -1,11 +1,19 @@
-'use client'
+"use client";
 
-const EditProfileButton = () => {
+import { useState } from "react";
+import EditProfileModal from "./editProfileModal";
+import { User } from "@supabase/supabase-js";
+
+const EditProfileButton = ({user}: {user: User | null}) => {
+  const [showModal, setShowModal] = useState<boolean>(false);
   return (
-    <button>
-      프로필 편집하기
-    </button>
-  )
-}
+    <>
+      <button onClick={() => {
+        setShowModal(prev => !prev);
+      }}>프로필 편집하기</button>
+      {showModal && <EditProfileModal user={user} setShowModal={setShowModal} />}
+    </>
+  );
+};
 
-export default EditProfileButton
+export default EditProfileButton;
