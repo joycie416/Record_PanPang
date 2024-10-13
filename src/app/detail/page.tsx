de5abac0ae12894e8a3e3a776be1eef536dbe6a3
hanamstarfield <qnfdmlto@naver.com>
2024-10-13 16:51:38 +0900

    [update] 댓글 기능 supabase 연결

---
+++ src/app/detail/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Detailpage = () => {
+  return <div>Detailpage</div>;
+};
+
+export default Detailpage;
---
+++ src/components/features/comment/CommentSection.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+
+const CommentSection = ({postId}: {postId:string}) => {
+
+  useEffect(()=>{
+    async function loadComments() {
+      const fetchedComments = await
+      
+    }
+  },[])
+
+
+  return (
+    <div>CommentSection</div>
+  )
+}
+
+export default CommentSection
