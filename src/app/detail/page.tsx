02aece1647d1ad5ce4fb2bcfe0b01901f40f2447
hanamstarfield <qnfdmlto@naver.com>
2024-10-13 18:35:11 +0900

    [update] 댓글 supabase 연결 로직 action으로 이동

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,7 +1,14 @@
+import CommentSection from "@/components/features/comment/CommentSection";
 import React from "react";
 
-const Detailpage = () => {
-  return <div>Detailpage</div>;
+const Detailpage = ({ params }: { params: { id: string } }) => {
+  const postId = "test-post-id";
+
+  return (
+    <div>
+      <CommentSection postId="test-post-id" />
+    </div>
+  );
 };
 
 export default Detailpage;
--- src/components/features/comment/CommentSection.tsx
+++ src/components/features/comment/CommentSection.tsx
@@ -1,18 +1,60 @@
-import React, { useEffect } from 'react'
+import { Comment } from "@/types/comment";
+import { addComment, deleteComment, fetchComment, updateComment } from "@/utils/supabase/actions";
+import React, { useEffect, useState } from "react";
 
-const CommentSection = ({postId}: {postId:string}) => {
+const CommentSection = ({ postId }: { postId: string }) => {
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [newComment, setNewComment] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     async function loadComments() {
-      const fetchedComments = await
-      
+      const fetchedComments = await fetchComment(postId);
+      setComments(fetchedComments);
     }
-  },[])
+    loadComments();
+  }, [postId]);
 
+  // 댓글 작성 핸들
+  const handleAddComment = async () => {
+    if (!newComment) return;
+
+    await addComment(newComment, postId);
+    setNewComment("");
+    const updatedComments = await fetchComment(postId);
+    setComments(updatedComments);
+  };
+
+  // 댓글 삭제 핸들
+  const handleDeleteComment = async (commentId: string) => {
+    await deleteComment(commentId);
+    const updateComments = await fetchComment(postId);
+    setComments(updateComments);
+  };
+
+  // 댓글 수정
+  const handleUpdateComment = async (commentId: string, content: string) => {
+    await updateComment(commentId, content);
+    const updatedComments = await fetchComment(postId);
+    setComments(updatedComments);
+  };
 
   return (
-    <div>CommentSection</div>
-  )
-}
+    <div>
+      <textarea name="newComment" placeholder="댓글을 입력하세요" onChange={(e) => setNewComment(e.target.value)} />;
+      <button onClick={handleAddComment}>추가</button>
+      <ul>
+        {comments.map((comment) => {
+          return (
+            <li key={comment.comment_id}>
+              <p>{comment.content}</p>
+              <button onClick={() => handleUpdateComment(comment.comment_id, "새로운 내용")}> 수정</button>
+              <button onClick={() => handleDeleteComment(comment.comment_id)}>삭제</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default CommentSection
+export default CommentSection;
