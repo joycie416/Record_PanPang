4da1de476852c71a7e79c1641554f5596b3ec096
joycie416 <joycie416@gmail.com>
2024-10-16 20:44:55 +0900

    [fix] auth 쪽 css 수정

--- src/components/features/navbar/Footer.tsx
+++ src/components/features/navbar/Footer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Footer = ({ className }: { className: string }) => {
+const Footer = () => {
   return (
-    <footer className={"w-full h-32 bg-gray-700 flex flex-col justify-center items-center text-gray-300 " + className}>
+    <footer className={"w-full h-32 bg-gray-700 flex flex-col justify-center items-center text-gray-300 "}>
       <h1>2조 송진우 이보영 정수희 조아영 조해인</h1>
       <h1>ⓒ 2024. 2조 All rights reserved.</h1>
     </footer>
